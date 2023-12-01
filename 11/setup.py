3913ff917569b46886c788f5459bedff51d90892
semyon <semen.ryaboff@yandex.ru>
2023-12-01 16:08:58 +0300

    hw 11

--- 11/setup.py
+++ 11/setup.py
@@ -2,15 +2,12 @@ from setuptools import setup, Extension
 
 
 def main():
-    setup(name="cjson",
-          version="1.0.1",
-<<<<<<< HEAD
-          author="Ryabov Semyon",
-=======
-          author="Stas Belosludcev",
->>>>>>> origin/main
-          ext_modules=[Extension('cjson', ['./cjson.c'])]
-          )
+    setup(
+        name="cjson",
+        version="1.0.1",
+        author="Ryabov Semyon",
+        ext_modules=[Extension("cjson", ["./cjson.c"])],
+    )
 
 
 if __name__ == "__main__":
